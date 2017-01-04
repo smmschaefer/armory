56b907f689c063d3da66a9ef10246f19e880924a
smmschaefer <sms8@pdx.edu>
2017-01-04 10:34:30 -0800

    pushing up framework

---
+++ app/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Main extends Component {
+	render() {
+		return(
+			<div>
+				Armory up and running test
+			</div>
+		)
+	}
+}
+
+
+ReactDOM.render(<Main />, document.getElementById('app'));
